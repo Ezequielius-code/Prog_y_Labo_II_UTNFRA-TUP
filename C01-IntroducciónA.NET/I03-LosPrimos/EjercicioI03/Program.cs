86c103f2e9bd0f66f7457fdd24149f2b236844f8
unknown <bruttoezequiel@gmail.com>
2022-03-22 18:16:20 -0300

    EjercicioI03Terminado

---
+++ C01-IntroducciónA.NET/I03-LosPrimos/EjercicioI03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EjercicioI03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "seguir";
+
+            do
+            {
+                Console.WriteLine("Para calcular números primos, ingrese un número.\n" +
+                                  "Si desea cerrar el programa, ingrese salir.");
+                answer = Console.ReadLine();
+
+                if (int.TryParse(answer, out int enteredNumber))
+                {
+                    int temp = 0;
+                    for (int i = 2; i <= enteredNumber; i++)
+                    {
+                        for (int j = 1; j <= i; j++)
+                        {
+                            if (i % j == 0)
+                                temp++;
+                        }
+                        if (temp <= 2)
+                        {
+                            Console.WriteLine("{0}", i);
+                        }
+                        temp = 0;
+                    }
+
+                    Console.WriteLine("\n¿Desea ingresar un nuevo número o salir?\n   Ingrese: seguir - salir");
+                    answer = Console.ReadLine();
+                }
+            } while (answer != "salir");
+        }
+    }
+}
