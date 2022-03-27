821cdc1dda3e4c20b6f15955fdf96876a64d657b
unknown <bruttoezequiel@gmail.com>
2022-03-27 06:36:12 -0300

    C02-I02Terminado

---
+++ C02-ClasesY_MétodosEstáticos/I02-DeseaContinuar/EjercicioI02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace EjercicioI02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char caracter;
+            bool respuesta = true;
+            int enteredNumber = 0;
+            bool validNumber = false;
+            int accumulator = 0;
+
+            Console.WriteLine("SUMADOR");
+            while (respuesta)
+            {
+                while (!validNumber)
+                {
+                    Console.WriteLine("Ingrese número a sumar: ");
+                    if (int.TryParse(Console.ReadLine(), out enteredNumber))
+                    {
+                        accumulator += enteredNumber;
+                        validNumber = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR. Ingreso no válido.");
+                    }
+                }
+
+                Console.WriteLine("¿Desea continuar? (S/N)");
+
+                caracter = Convert.ToChar(Console.ReadLine());
+                if (Validador.ValidarRespuesta(caracter))
+                {
+                    respuesta = true;
+                    validNumber = false;
+                }
+                else
+                {
+                    respuesta = false;
+                }
+            }
+
+            Console.Clear();    
+            Console.WriteLine($"La suma total de los números ingresados es: {accumulator}");
+        }
+    }
+}
---
+++ C02-ClasesY_MétodosEstáticos/I02-DeseaContinuar/EjercicioI02/Validador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioI02
+{
+    internal class Validador
+    {
+        public static bool ValidarRespuesta(char respuesta)
+        {
+            if (respuesta == 'S')
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
