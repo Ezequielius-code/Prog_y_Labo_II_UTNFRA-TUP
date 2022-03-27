0b7c9620a769e5e0409d49adbdc1aa39b98ab64b
unknown <bruttoezequiel@gmail.com>
2022-03-27 05:10:39 -0300

    EjercicioI08Completo

---
+++ C01-IntroducciónA.NET/I08-TrianguloRectangulo/EjercicioI08/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EjercicioI08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte floors = 0;
+
+            Console.WriteLine("Right-angled triangle.");
+            Console.WriteLine("Enter the number of floors for your triangle: ");
+
+            if (byte.TryParse(Console.ReadLine(), out floors))
+            {
+                Console.WriteLine("\n");
+                for (int i = 1; i <= floors; i++)
+                {
+                    for (int j = 1; j < i * 2; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine("\n");
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nERROR. Closing app...");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
