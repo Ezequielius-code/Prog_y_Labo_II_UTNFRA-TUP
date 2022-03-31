b72ac7936be5df1b0a4a9fa7943b8157bedd75b3
unknown <bruttoezequiel@gmail.com>
2022-03-31 18:19:04 -0300

    NuevosEjerciciosCompletos

---
+++ C02-ClasesY_MétodosEstáticos/I04-LaCalculadora/EjercicioI04/Calculadora.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioI04
+{
+    internal class Calculadora
+    {
+        public static double Calcular(double operando1, double operando2, char operador)
+        {
+            double result = 0;
+
+            switch (operador)
+            {
+                case '+':
+                    result = operando1 + operando2;
+                    break;
+                case '-':
+                    result = operando1 - operando2;
+                    break;
+                case '*':
+                    result = operando1 * operando2;
+                    break;
+                default:
+                    if (Validar(operando2))
+                    {
+                        result = operando1 / operando2;
+                    }
+                    break;
+            }
+            return result;
+        }
+
+        private static bool Validar(double divisor)
+        {
+            if (divisor != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ C02-ClasesY_MétodosEstáticos/I04-LaCalculadora/EjercicioI04/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace EjercicioI04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double operando1 = 0;
+            double operando2 = 0;
+            char operador = '_';
+            double resultado = 0;
+            char respuesta = 'S';
+            bool validarRespuesta = true;
+            bool ingresoValido = false;
+
+            while (validarRespuesta)
+            {
+                while (!ingresoValido)
+                {
+                    Console.WriteLine("Ingrese el primer operando: ");
+                    if (double.TryParse(Console.ReadLine(), out operando1))
+                    {
+                        ingresoValido = true;
+                        break;
+                    }
+                    Console.WriteLine("ERROR. Ha ingresado un dato inválido.\n");
+                }
+
+                ingresoValido = false;
+                while (!ingresoValido)
+                {
+                    Console.WriteLine("Ingrese el segundo operando: ");
+                    if (double.TryParse(Console.ReadLine(), out operando2))
+                    {
+                        ingresoValido = true;
+                        break;
+                    }
+                    Console.WriteLine("ERROR. Ha ingresado un dato inválido.\n");
+                }
+
+                ingresoValido = false;
+                while (!ingresoValido)
+                {
+                    Console.WriteLine("Ingrese la operación a realizar (+ | - | * | /): ");
+                    operador = Convert.ToChar(Console.ReadLine());
+                    if (operador == '+' || operador == '-' || operador == '*' || operador == '/')
+                    {
+                        ingresoValido = true;
+                        break;
+                    }
+                    Console.WriteLine("ERROR. Ha ingresado un dato inválido.\n");
+                }
+
+                resultado = Calculadora.Calcular(operando1, operando2, operador);
+                Console.WriteLine($"\nResultado de la operación: {operando1} {operador} {operando2} = {resultado}");
+                
+                Console.WriteLine("\n¿Desea realizar otra operación? (S / N)");
+                respuesta = Convert.ToChar(Console.ReadLine());
+                if (Validador.ValidarRespuesta(respuesta))
+                {
+                    validarRespuesta = true;
+                    ingresoValido = false;
+                }
+                else
+                {
+                    validarRespuesta = false;
+                }
+            }
+        }
+    }
+}
